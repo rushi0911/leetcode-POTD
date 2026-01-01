226cee892b2993bfc48adbb37bd3f4caf89cc61e
Rushikesh Manurkar <83496076+rushi0911@users.noreply.github.com>
2026-01-01 16:41:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (50.17%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        vector<int>ans;
+        // int carry = 0;
+        // digits[n-1] += 1;
+        int carry = 1;
+        for(int i=n-1;i>=0;i--){
+            digits[i] += carry;
+            carry = digits[i] / 10;
+            digits[i] %= 10;
+            if(carry == 0) break;
+        }
+        if(carry){
+            digits.resize(n+1);
+            digits[0] = carry;
+            for(int i=n;i>1;i--){
+                digits[i] = digits[i-1];
+            }
+        }
+        
+        return digits;
+    }
+};
