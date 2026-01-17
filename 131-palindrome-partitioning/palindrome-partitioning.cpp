c90b276e1ca76bf8a24451603dd992a604acd539
Rushikesh Manurkar <83496076+rushi0911@users.noreply.github.com>
2026-01-17 12:39:41 +0530

    Time: 151 ms (8.13%) | Memory: 149.1 MB (7.89%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool checkPal(string s,int l,int h){
-        while(l < h){
+        while(l <= h){
             if(s[l] != s[h]){
                 return false;
             }
@@ -10,24 +10,25 @@ public:
         }
         return true;
     }
-    void solve(int idx,int n,string s,vector<string>temp,vector<vector<string>>&ans){
-        if(idx  == n){
-            ans.push_back(temp);
+    void solve(int idx,int n,string s,vector<vector<string>>&ans,vector<string>str){
+        if(idx == n){
+            ans.push_back(str);
             return ;
         }
         for(int i=idx;i<n;i++){
             if(checkPal(s,idx,i)){
-                temp.push_back(s.substr(idx,i-idx+1));
-                solve(i+1,n,s,temp,ans);
-                temp.pop_back();
+                string temp = s.substr(idx,i-idx+1);
+                str.push_back(temp);
+                solve(i+1,n,s,ans,str);
+                str.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
         int n = s.size();
         vector<vector<string>>ans;
-        vector<string>temp;
-        solve(0,n,s,temp,ans);
+        vector<string>str;
+        solve(0,n,s,ans,str);
         return ans;
     }
 };
